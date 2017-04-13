192e9ce5e77c2ef40c2216ec3ccc0c57100203ec
Istvan <Voidnote@Istvans-MacBook-Pro.local>
2017-04-12 21:14:07 -0700

    Fixed flash bug in the validation process

--- email_validation/server.py
+++ email_validation/server.py
@@ -6,37 +6,38 @@ mysql = MySQLConnector(app,"email_registration")
 app.secret_key = "ThisIsSecret"
 EMAIL_REGEX = re.compile(r'^[a-zA-Z0-9.+_-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]+$')
 
-
+# renders the main index page
 @app.route("/")
 def index():
     return render_template("index.html")
 
-# def index():
-#     query = "SELECT name, age, DATE_FORMAT(created_at, '%M %D') as date, DATE_FORMAT(created_at, '%Y') as year FROM friends"
-#     friends = mysql.query_db(query)
-#     return render_template("index.html", all_friends = friends)
-
+# validates the email address that was entered, and sends it to the DB
 @app.route("/new", methods=["POST"])
 def new():
+    session["email"] = request.form["email"]
+    myEmail = session["email"]
     if not EMAIL_REGEX.match(request.form['email']):
         flash("Invalid Email Address!")
         return redirect("/")
     else:
-        flash("Thank you for registering")
+        flash("The email address you entered ")
+        flash(session["email"])
+        flash(" is a VALID email address. Thank you.")
         query = "INSERT INTO emails (email, created_at, updated_at) VALUES (:email, NOW(), NOW())"
-        session["email"] = request.form["email"]
         data = {
-            'email': request.form['email'],
+            'email': session["email"],
            }
         mysql.query_db(query, data)  
     return redirect("/success")
 
+# displays all of the email addresses entered
 @app.route("/success")
 def success():
     query = "SELECT email, DATE_FORMAT(created_at, '%m/%d/%Y %h:%i%p') as date, id FROM emails"
     emails = mysql.query_db(query)
     return render_template("results.html", emails = emails)
 
+# deletes the chosen email address
 @app.route("/delete/<id>", methods=["POST"])
 def delete(id):
     query = "DELETE FROM emails WHERE id=:id"
